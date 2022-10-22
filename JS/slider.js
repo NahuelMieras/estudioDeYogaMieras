72dda53b01f0c791a00e439d3767e24eefc5ef2c
Nahuel Mieras <nahuelmieras15@gmail.com>
2022-10-22 12:52:06 -0300

    agregue questions y footer

---
+++ JS/slider.js
@@ -0,0 +1,28 @@
+(function(){
+
+    const sliders = [...document.querySelectorAll(".profes__body")]
+    const buttonNext = document.querySelector("#next");
+    const buttonBefore = document.querySelector("#before");
+    let value;
+
+    buttonNext.addEventListener("click", ()=>{
+        changePosition(1);
+    });
+
+    buttonBefore.addEventListener("click",()=>{
+        changePosition(-1);
+    })
+
+    const changePosition =(add)=>{
+        const currentProfes =document.querySelector(".profes__body--show").dataset.id;
+        value = Number(currentProfes);
+        value += add;
+        
+        sliders[Number(currentProfes)-1].classList.remove("profes__body--show");
+        if(value === sliders.length+1 || value === 0){
+            value = value === 0 ? sliders.length : 1
+        }
+
+        sliders[value-1].classList.add("profes__body--show");
+    } 
+})();
